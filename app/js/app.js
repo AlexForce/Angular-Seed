1eb9c7c4b78c8f31fc237d3da344f62cad59574a
Solomon Perkins <falexson@gmail.com>
2013-11-14 17:12:33 -0400

    Started working on routing

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,24 @@
 
 
 //Create the module to be used 
-var eventsApp= angular.module('eventsApp', []);
+var eventsApp= angular.module('eventsApp', ['ngRoute']);
 	
+
+eventsApp.config(['routeProvider',
+	function(routeProvider){
+		$routeProvider
+			.when('/', {template: 'partials/home.html', controller: })
+			.when('/new', {
+				templateUrl: 'new.html',
+				controller: 'NewCtrl'
+			})
+			.when('/login', {
+				templateUrl: 'login.html',
+				controller: 'LoginCtrl'
+			});
+
+
+	}]);
+
 
 
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,63 +1,37 @@
 'use strict';
 
 //The list of modules which will be used
-eventsApp.controller("AppCtrl", 
-                function AppCtrl(){
+eventsApp.controller("AppCtrl",
+    function AppCtrl(){
+              $scope.events=[{
+                        id: 1,
+                        name:"Solomon",
+                        imageUrl:"img/default.png",
+                        date: "2013-12-12",
+                        time: "12:12:00",
+                        description: "A simple event trial",
+                        status:'label-warning'
+                },
+                {        id: 2,
+                        name:"Peter",
+                        imageUrl:"img/car.jpg",
+                        date: "2013-12-12",
+                        time: "12:12:00",
+                        description: "A simple event trial",
+                        status:'label-success'
+                },
+                {
+                        id: 3,
+                        name:"Medullan Event",
+                        imageUrl: 'img/default.png',
+                        date: '2014-12-12',
+                        time: '1:1:1',
+                        description: 'A simple event to ... not sure as yet',
+                        status:'label-important'
+                }
+                ];
+                
+
                         
-                        var promise= Kinvey.init({
-                                appKey: 'kid_VeqEMPqrmO',
-                                appSecret: 'dc714511920f40baa8cad20588ca418b'
-                        });
-                        
-                        var activeUser;
-                        
-                        
-                        
-                        
-                        promise.then(function(activeUser){
-                                console.log(activeUser);
-                                        
-                                         //Verify that the application is connect to server 
-                                var ping= Kinvey.ping();
-                                ping.then(function(response){
-                                        console.log("Ping worked:");
-                                        console.log(response);
-                                        
-                                        
-                                        
-                                      
-                                }, function(error){
-                                        console.error(error);
-                                        
-                                });
-                                
-                                
-                                
-                              //Creating an active user
-                              activeUser = Kinvey.User.signup({
-                                                username: 'AlexForce',
-                                                password: 'AlexForce1'          
-                                           }, {
-                                              success: function(response){
-                                                      alert("Login in");
-                                                      console.log(response);
-                                              
-                                                      
-                                              }
-                                           });
-                                           
-                            //Save data
-                             db= Kinvey.DataStore.save("trial", {
-                                      _id:1,
-                                      prop:'value'
-                              }, {
-                                   sucess: function(reponse){
-                                           alert("data saved");
-                                           console.log(response);
-                                   }
-                              });
-                                
-                        }, function (error){
-                                console.error(error);
-                        });
-     });
+                       
+    });
